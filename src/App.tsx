44e35f37a059749a28cbf8e082872e7785c0ecd3
John M. Shea <jshea@ece.ufl.edu>
2022-03-07 12:23:49 -0500

    Update name and meta to indicate that includes country flags

--- src/App.tsx
+++ src/App.tsx
@@ -88,7 +88,7 @@ function App() {
               <InstallButton pwaInstall={pwaInstall} />
             )}
             <h1 className="text-4xl font-bold uppercase tracking-wide text-center my-1 flex-auto">
-              Wor<span className="text-green-600">l</span>dle
+              Wor<span className="text-green-600">l</span>dle+Flags
             </h1>
             <button
               className="ml-3 text-xl"
